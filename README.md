ef603e1df7e6d42f9f861de7f29f1ff510732125
Dan Weikart <danmweikart@gmail.com>
2020-03-28 23:00:30 -0700

    homework draft

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # JavaScript_Quiz
+
+I created a simple quiz to employ the DOM manipulation techniques I recently learned.
+
+## Languages Used
+
+Using Bootstrap as a starting point, I created the HTML to house the JavaScript.
+
+CSS was used to style it a bit more.
+
+JavaScript was the main language of project as it makes the page interactive. I created an array of questions and by manipulating the DOM was able to sequentially display each question object.
+
+## Description
+
+Upon loading the page shows a home screen to introduce the quiz as well as a Start button to begin the question sequence.
+
+
+
+Once the Start button is pressed the home screen becomes hidden and the first of five questions appears. The answer options are displayed as buttons that, when clicked, will advance the user to the next question. A timer is also displayed which begins counting down from 75 seconds.
+
+
+
+Each wrong answer deducts 10 seconds from the timer. The game is over once all five questions have been answered and the user's score is however many seconds are left on the clock. There is a game end screen to show the user their score and allow them to input their initials.
+
+
+*There was supposed to be a way to save their initials and score into a Highscore screen, but I could not work out the bugs in time. I will continue working on it and will be able to upload a final product at some point.
+
+
+## Acknowledgement
+
+Author Dan Weikart would like to thank everyone in his UW Coding Bootcamp class for helping improve my understanding of HTML, CSS, and especially JavaScript.
+
+Special shoutout to instructor John Young and TA's Abdul Aziz and Ben Vaagen.
+
+
--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 var startButton = document.getElementById("start-quiz");
-var cardBodyEl = document.querySelector(".card-body");
-var cardTitleEl = document.querySelector(".card-title");
-var cardTextEl = document.querySelector(".card-text");
 var start = document.getElementById("start-screen");
 var firstQ = document.getElementById("firstQuestion");
 var currQuest = document.getElementById("question");
 var choiceEl = document.getElementById("questionChoice");
-var time = 75;
+var highScoreScreen = document.getElementById("highscore-screen");
+var hiScoreList = document.getElementById("highscore-list")
+var time;
 var questionIndex = 0;
 var score = 0;
 // need to pull from local storage
 var highscore = 0;
-// var currentTime = time;
+
 
 var questionList = [
     //question 1
     {
         title: "Commonly used data types DO NOT include:",
         choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        answer: "1. alerts"
+        answer: "3. alerts"
     },
     //question 2
     {
@@ -46,138 +45,73 @@ var questionList = [
     }
 ]
 
-
-function populateQuestion () {
-    // currentQuestion gets object of questionList array (starting at index [0]) 
-    var currentQuestion = questionList[questionIndex];
-    // set textContent of <h3> with ID question to title of currentQuestion
-    currQuest.textContent = currentQuestion.title;
-    // forEach loop which takes input of choice and index number
-    currentQuestion.choices.forEach(function(choice,i){
-        //create button
-        var choiceButton = document.createElement("button");
-        // set value of button to choice
-        choiceButton.setAttribute("value", choice);
-        // set class of button to apply CSS styles
-        choiceButton.setAttribute("class", "btn choicebtn");
-        // set text of button to current choice
-        choiceButton.textContent = choice;
-        // displaying on HTML page
-        choiceEl.appendChild(choiceButton);       
-    });
-    // correct gets the answer object from each question
-    // var correct = questionList[questionIndex].answer;
-    //     console.log(correct);
-
-    //     // want to set attribute of correct response to true...
-    //     // how do I 
-    //     console.log(questionList[questionIndex].choices[i]);
-    // if (questionList[questionIndex].choices[i] === correct) {
-    //     choiceButton.setAttribute("true", "true");
-    // }
-} 
-// when element inside choiceEl is clicked
-choiceEl.addEventListener("click", function(event){
-    var element = event.target;
-    // if element is button
-    if (element.matches("button") === true) {
-        //run scoring function
-
-        scoring();
-        //update current question by increasing questionIndex by one
-        questionIndex++;
-        //remove everything in innerHTML to make room for new question
-        questionChoice.innerHTML = "";
-        if (questionIndex < questionList.length){
-            // if there are still more questions, populate questions
-            populateQuestion();
-            // else, go to end score
-        // } else {
-            // endScore();
-
-        }
-        
-    }
-
-});
-
-function scoring () {
-
-//1. listen for button click
-//2. figure out which button clicked
-//3. get text of button
-//4. check if text of button matches answer
-//5. if does not match: time = time -10;
-
-
-// when element inside choiceEl is clicked
-choiceEl.addEventListener("click", function(event){
-    var element = event.target;
-    // if element is button
-    if (element.matches("button") === true) {
-    console.log(element.textContent);
-   
-        // if (element.textContent === questionList[questionIndex].answer){
-        //     console.log("correct");
-        // }
-    console.log(questionList[questionIndex].answer)    
-    console.log(element.textContent === questionList[questionIndex].answer);    
-    
-
-    }
-});
-}
-
-// function endScore () {
-//     //1. replace .card-title text with "All done!"
-//     var scoreTitle = cardTitleEl.textContent = "All done!";
-//     //2. replace .card-text with "Your final score is + time"
-//     var scoreText = cardTextEl.innerHTML = "<p>'Your final score is ' + time + '.'</p><br> Enter Initials: ";
-//     //3. add form to enter initials
-//     // var initials = cardTitleEl.createElement("form"); 
-//     //4. add submit button that takes us to highscrore screen
-
-
-//     cardTitleEl.appendChild(scoreTitle);
-//     cardTextEl.appendChild(scoreText);
-//     // cardTextEl.appendChild(initails);
-// }
-
-// create countdown timer function
-function timer () {
-
+function timer() {
     start.setAttribute("class", "hide");
-
     firstQ.removeAttribute("class");
-    // start time at 75 seconds
     time = 75;
-    // assign timer element to <div class="time">
     var timerEl = document.querySelector(".time");
-    // set the text content to "Time: " and display the seconds remaining
     timerEl.textContent = "Time: " + time;
-    // countdown function
-    var countdown = setInterval(function() {
-        // decrease time var by one
+    countdown = setInterval(function () {
         time--;
-
         timerEl.textContent = "Time: " + time;
-        // when time reaches zero
-        if (time === 0) {
-            // change text in timer element to 0
-            timerEl.textContent = "Time: 0";
-            // clear countdown interval
-            clearInterval(countdown);
-            // begin endScore function
-            // endScore();
-        }
-        // set interval at 1000 ms which = 1 second
     }, 1000)
-
-    populateQuestion();
+    renderQuestion(questionList[questionIndex]);
 }
-// call timer function
 
-// startButton.onclick = timer;
+function renderQuestion(currentQuestion) {
+  
+    currQuest.textContent = currentQuestion.title;
+
+    currentQuestion.choices.forEach(function (choice, i) {
+        var choiceButton = document.createElement("button");
+        choiceButton.setAttribute("value", choice);
+        choiceButton.setAttribute("class", "btn choicebtn");
+        choiceButton.textContent = choice;
+        choiceEl.appendChild(choiceButton);
+    });
+}
+
+choiceEl.addEventListener("click", function (event) {
+    var element = event.target;
+
+    if (element.matches("button") === true) {
+        console.log(element.textContent);
+        console.log(questionList[questionIndex].answer)
+        questionChoice.innerHTML = "";
+            if (element.textContent !== questionList[questionIndex].answer){
+                time = time - 10;
+            } if (questionIndex < questionList.length-1) {
+                questionIndex++;
+            renderQuestion(questionList[questionIndex]);
+            } else {
+                endScore();
+            }
+    }
+});
+
+function endScore () {
+    score = time;
+    clearInterval(countdown);
+    questionChoice.innerHTML = ""
+    currQuest.innerHTML = "";
+    var scoreScreen = document.getElementById("score-screen");
+    scoreScreen.setAttribute("class", "unhide")
+    var scoreText = document.getElementById("score-text");
+    scoreText.textContent = "Your final score is " + score + ".";
+} 
+
+function highscore () {
+    var scoreScreen = document.getElementById("score-screen");
+    scoreScreen.setAttribute("class", "hide")
+    var highscoreScreen = document.getElementById("highscore-screen");
+    highscoreScreen.setAttribute("class", "unhide")
+    scoreName = document.createElement("li");
+    scoreName.textContent = "New Score: " + score;
+    hiScoreList.append(scoreName);
+}
+
 startButton.addEventListener("click", timer);
-
-
+highScoreScreen.addEventListener("click", function(event){
+    event.preventDefault();
+    highscore;
+});
